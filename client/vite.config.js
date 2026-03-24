8c184fd4e59d5523146c86d8f63a82dd790319bb
1'm s0rry <50732046+75409885@users.noreply.github.com>
2026-03-24 13:34:45 +0800

    添加vuedevtool便于管理和查找问题

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import VueDevTools from 'vite-plugin-vue-devtools'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+    VueDevTools(),
   ],
   resolve: {
     alias: {
